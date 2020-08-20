e0098f63699d3ec73b3787ce7410970f79a9273f
Darya Baranouskaya <darya.baranovskaya01082001@gmail.com>
2020-08-20 17:30:33 +0000

    Time changes

---
+++ src/main/java/com/google/sps/src/Time.java
@@ -0,0 +1,29 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar; 
+
+public class Time {
+  private Calendar calendar;
+
+  public Time() {
+    calendar = Calendar.getInstance());
+  }
+
+  public Time(int year, int month, int date, int hour, int minute) {
+    calendar = Calendar.getInstance());
+    calendar.set(Calendar.YEAR, year);
+    calendar.set(Calendar.MONTH, month);
+    calendar.set(Calendar.DATE, date);
+    calendar.set(Calendar.HOUR, hour);
+    calendar.set(Calendar.MINUTE, minute);
+  }
+
+  public DATE getTime(int year, int month, int date, int hour, int minute) {
+    return calendar.getTime()
+  }
+}
