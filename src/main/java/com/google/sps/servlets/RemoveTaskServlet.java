786bff268fa32e02e2ae8fb38b8ad787b01f6bd4
Gor1x <ea-post134@yandex.ru>
2020-08-30 12:13:41 +0500

    Split code of doPost into different function

--- src/main/java/com/google/sps/servlets/RemoveTaskServlet.java
+++ src/main/java/com/google/sps/servlets/RemoveTaskServlet.java
@@ -26,36 +26,45 @@ import java.util.Scanner;
 
 @WebServlet("/remove-task")
 public class RemoveTaskServlet extends HttpServlet {
- 
+
+  private void doEditTask(HttpServletRequest request, HttpServletResponse response, long number) {
+    Key key = KeyFactory.createKey("task", number);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    try {
+      Entity entity = datastore.get(key);
+      HashMap<String, String> viewToProperty = new HashMap();
+      viewToProperty.put("task_titleData", "text");
+      viewToProperty.put("task_timeData", "time");
+      viewToProperty.put("task_placeData", "place");
+      viewToProperty.put("task_commentData", "comment");
+
+      String newFieldData = request.getParameter("new_data");
+      entity.setProperty(viewToProperty.get(request.getParameter("field")), newFieldData);
+      datastore.put(entity);
+    } catch (Throwable e) {
+      System.out.println("Key doesn't exists");
+    }
+  }
+
+  private void doDeleteTask(HttpServletRequest request, HttpServletResponse response, long number) {
+    Key key = KeyFactory.createKey("task", number);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.delete(key);
+  }
+
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     long number = Long.parseLong(request.getParameter("number"));
-    Key key = KeyFactory.createKey("task", number);
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     System.out.println("LOG: doPost /remove-task request");
     System.out.println("LOG: number of the item is " + number);
     System.out.println("LOG: type of the request is " + request.getParameter("type"));
 
     if (request.getParameter("type").equals("edit")) {
-      try {
-        Entity entity = datastore.get(key);
-        HashMap<String, String> viewToProperty = new HashMap();
-        viewToProperty.put("task_titleData", "text");
-        viewToProperty.put("task_timeData", "time");
-        viewToProperty.put("task_placeData", "place");
-        viewToProperty.put("task_commentData", "comment");
-
-        String newFieldData = request.getParameter("new_data");
-        entity.setProperty(viewToProperty.get(request.getParameter("field")), newFieldData);
-        datastore.put(entity);
-      } catch (Throwable e) {
-        System.out.println("Key doesn't exists");
-      }
-      response.sendRedirect("/index.html");
+      doEditTask(request, response, number);
     } else {
-      datastore.delete(key);
-      response.sendRedirect("/index.html");
+      doDeleteTask(request, response, number);
     }
+    response.sendRedirect("/index.html");
   }
 }
