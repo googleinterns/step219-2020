5836a4c1e571a8bde7e00c3242a8ce70ec99d1fa
Darya Baranouskaya <darya.baranovskaya01082001@gmail.com>
2020-09-11 22:53:47 +0000

    add google sign-in

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -495,7 +495,7 @@ function handleLocationError(browserGeoState, infoWindow, pos) {
 function showHideMap() {
   mapCurState = document.getElementById('map');
   mapPanel = document.getElementById('floating-panel');
-  taskForm = document.getElementById('task-form');
+  taskForm = document.getElementById('task-list-full-container');
   taskContainer = document.getElementById('task-container');
   if (mapCurState.style.display === "none") {
     initMap();
@@ -509,5 +509,29 @@ function showHideMap() {
     taskForm.style.display = "block";
     taskContainer.style.display = "block";
   }
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
+
+function onSignIn(googleUser) {
+// Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+ window.location.replace('https://8080-17f5303d-2dea-4c50-b733-2cb7b78be97f.europe-west4.cloudshell.dev/main-page.html');
 }
 
